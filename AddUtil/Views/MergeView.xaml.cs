111d519b42915293cdcfba9f5959cc1895280dda
mivafoxy <creatox1996@gmail.com>
2019-04-30 23:59:03 +0300

    Сделано окно для копирования старой базы данных.

---
+++ AddUtil/Views/MergeView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AddUtil.Views
+{
+    /// <summary>
+    /// Interaction logic for MergeView.xaml
+    /// </summary>
+    public partial class MergeView : Window
+    {
+        public MergeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
