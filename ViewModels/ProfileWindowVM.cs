fb21d4a2dd5ddb3a6a2901398caf1d7e052eb01f
Tenshow <tenshowviii@gmail.com>
2019-05-09 12:51:17 +0300

    Сделано окно анкеты для поздравления, добавлены проверки на заполненния полей

--- ViewModels/ProfileWindowVM.cs
+++ ViewModels/ProfileWindowVM.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace SeCongratulator.ViewModels
 {
@@ -18,6 +19,9 @@ namespace SeCongratulator.ViewModels
         private string selectedHoliday;
         private bool isMale;
         private bool isFemale;
+        private SolidColorBrush nameColor;
+        private float nameOpacity;
+        /*private SystemColors bgCol =*/
         public ProfileWindowVM()
         {
             User = new Profile();
@@ -26,6 +30,7 @@ namespace SeCongratulator.ViewModels
             SelectedInterest = String.Empty;
             IsMale = false;
             IsFemale = false;
+            NameOpacity = 1;
         }
         /// <summary>
         /// Очистка формы
@@ -36,12 +41,14 @@ namespace SeCongratulator.ViewModels
             {
                 return new DelegateCommand(new Action(() =>
                 {
+                    //MessageBox.Show(User.Name);
                     User.Name = String.Empty;
                     IsMale = false;
-                    isFemale = false;
+                    IsFemale = false;
                     SelectedAge = String.Empty;
-                    SelectedHoliday = String.Empty;
                     SelectedInterest = String.Empty;
+                    SelectedHoliday = null;
+                    NameColor = new SolidColorBrush(Color.FromArgb(100, 255, 255, 255));
                     MessageBox.Show("Анкета очищена!");
                 }));
             }
@@ -71,11 +78,32 @@ namespace SeCongratulator.ViewModels
             }
         }
 
+        public ICommand CheckValid
+        {
+            get
+            {
+                return new DelegateCommand(new Action(() =>
+                {
+                    //NameOpacity = 0.3f;
+                    if (User.Name != null)
+                    {   
+                        NameColor = new SolidColorBrush(Color.FromArgb(100,0,200,0));
+                    }
+                    if (User.Name == String.Empty) 
+                    {
+                        NameColor = new SolidColorBrush(Color.FromArgb(100, 200, 0, 0));
+                    }
+                }));
+            }
+        }
+
         public Profile User { get => user; set => SetField(ref user, value); }
         public string SelectedAge { get => selectedAge; set => SetField(ref selectedAge, value); }
         public bool IsMale { get => isMale; set => SetField(ref isMale, value); }
         public bool IsFemale { get => isFemale; set => SetField(ref isFemale, value); }
         public string SelectedInterest { get => selectedInterest; set => SetField(ref selectedInterest, value); }
         public string SelectedHoliday { get => selectedHoliday; set => SetField(ref selectedHoliday, value); }
+        public SolidColorBrush NameColor { get => nameColor; set => SetField(ref nameColor, value); }
+        public float NameOpacity { get => nameOpacity; set => SetField(ref nameOpacity, value); }
     }
 }
