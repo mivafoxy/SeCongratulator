0d71d01fc3c4fa6e0460797b706a31cef5799612
mivafoxy <creatox1996@gmail.com>
2019-05-12 23:38:03 +0300

    Проектирование моделей для миграции старой базы в новую

---
+++ AddUtil/Models/OldDbModels/OldComgratulationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddUtil.Models.OldDbModels
+{
+    public class OldComgratulationModel : ModelBase
+    {
+    }
+}
